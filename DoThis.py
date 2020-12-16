e75e5080fad0cd36c10f59f25ad89fbad308c087
cooperroberts11 <73903206+cooperroberts11@users.noreply.github.com>
2020-12-16 12:10:11 -0500

    Final

--- DoThis.py
+++ DoThis.py
@@ -2,3 +2,9 @@
 # ask the user for two numbers they want added,
 # add them together and return the result. 
 # then commit and push it back up to github.
+
+print("Cooper Roberts")
+num1=float(input("Enter the first number you want to add: "))
+num2=float(input("Enter the second number you want to add: "))
+sum=num1+num2
+print(str(sum))
